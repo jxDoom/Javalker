1994756de36ddb92cbd90ac82cdaaa2c709aaa08
Alexey Amanov <chelsea.alex@list.ru>
2023-09-09 01:41:52 +0300

    added second lecture

--- SecondLecture/src/Main.java
+++ SecondLecture/src/Main.java
@@ -1,17 +1,64 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//Функционал API для строк:
+//        concat(): объединение строк
+//        valueOf(): преобразует Object в строковое представление (завязан на toString())
+//        join(): объединяет набор строк в одну с учетом разделителя
+//        charAt(): получение символа по индексу
+//        indexOf(): первый индекс вхождения подстроки
+//        lastIndexOf(): последний индекс вхождения подстроки
+//        startsWith()/endsWith(): определяет, начинается/заканчивается ли строка с подстроки
+//        replace(): замена одной подстроки на другую
+//        trim(): удаляет начальные и конечные пробелы
+//        substring(): возвращает подстроку, см.аргументы
+//        toLowerCase()/toUpperCase(): возвращает новую строку в нижнем/верхнем регистре
+//        сompareTo(): сравнивает две строки
+//        equals(): сравнивает строки с учетом регистра
+//        equalsIgnoreCase(): сравнивает строки без учета регистра
+//        regionMatches(): сравнивает подстроки в строках
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        /*String[] name = { "C", "е", "р", "г", "е", "й" };
+        String sk = "СЕРГЕЙ КА.";
+        System.out.println(sk.toLowerCase()); // сергей ка.
+        System.out.println(String.join("", name)); // Cергей
+        System.out.println(String.join("", "C", "е", "р", "г", "е", "й")); // Cергей
+        System.out.println(String.join(",", "C", "е", "р", "г", "е", "й")); // C,е,р,г,е,й*/
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+//Работа с файловой системой. Файлы
+//        Для работы нужно:
+//        File <имя> = new File(<полный путь к файлу>);
+//        File f1 = new File("file.txt");
+//        File f2 = new File("/Users/sk/vscode/java_projects/file.txt");
+
+//        String pathProject = System.getProperty("user.dir");
+//        String pathFile = pathProject.concat("/file.txt");
+//        File f3 = new File(pathFile);
+//        System.out.println(f3.getAbsolutePath ());
+//        // /Users/sk/vscode/java_projects/file.txt
+//        // C:/Users/Sk/Documents/xxx/brainexplosion/java/file.txt
+
+//Ошибки при работе с файловой системой
+//        try {
+//            Код, в котором может появиться ошибка
+//        } catch (Exception e) {
+//            Обработка, если ошибка случилась
+//        }
+//        finally {
+//            Код, который выполнится в любом случае
+//        }
+
+//Функционал API для работы с файловой системой:
+//        isHidden(): возвращает истину, если каталог или файл является скрытым
+//        length(): возвращает размер файла в байтах
+//        lastModified(): возвращает время последнего изменения файла или каталога
+//        list(): возвращает массив файлов и подкаталогов, которые находятся в каталоге
+//        listFiles(): возвращает массив файлов и подкаталогов, которые находятся
+//        в определенном каталоге
+//        mkdir(): создает новый каталог
+//        renameTo(File dest): переименовывает файл или каталог
+
+
     }
 }
