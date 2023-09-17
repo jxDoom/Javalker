a50ab55607e7e540b9f8063d7aa53901a0806a91
Alexey Amanov <chelsea.alex@list.ru>
2023-09-18 01:07:17 +0300

    created fourth lecture

---
+++ FourthLecture/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) {
+//LinkedList
+//        LinkedList<Integer> ll = new LinkedList<Integer>();
+//
+//        ll.add(1);
+//        ll.add(2);
+//        ll.add(3);
+
+//Queue
+//        Queue<Integer> queue = new LinkedList<Integer>();
+//        queue.add(1);
+//        queue.add(2);
+//        queue.add(3);
+//        queue.add(15);
+//        queue.add(144);
+//        int item = queue.remove();  //[2, 3, 15, 144]
+//        item = queue.remove();  //[3, 15, 144]
+//        queue.add(4);   //[3, 15, 144, 4]
+//        System.out.println(queue);
+
+
+    }
+}
