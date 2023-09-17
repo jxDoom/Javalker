1b96658bca72e249ff43918d1289de70b37993e3
Alexey Amanov <chelsea.alex@list.ru>
2023-09-17 22:56:31 +0300

    initial commit

---
+++ SecondHomeWork/src/FirstTask/Main.java
@@ -0,0 +1,8 @@
+package FirstTask;
+
+public class Main {
+        public static StringBuilder answer(String QUERY, String PARAMS){
+
+    }
+}
+
---
+++ SecondHomeWork/src/FirstTask/Printer.java
@@ -0,0 +1,22 @@
+package FirstTask;
+
+public class Printer {
+
+    public static void main(String[] args) {
+        String QUERY = "";
+        String PARAMS = "";
+
+        if (args.length == 0) {
+            // При отправке кода на Выполнение, вы можете варьировать эти параметры
+            QUERY = "select * from students where ";
+            PARAMS = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"} ";
+        }
+        else{
+            QUERY = args[0];
+            PARAMS = args[1];
+        }
+
+        Main ans = new Main();
+        System.out.println(ans.answer(QUERY, PARAMS));
+    }
+}
