bd0dd0f8c782803a1b466b7b24a0330ff884575b
Alexey Amanov <chelsea.alex@list.ru>
2023-10-01 23:42:30 +0300

    added 5 seminar

--- FifthSeminar/src/Cat.java
+++ FifthSeminar/src/Cat.java
@@ -1,16 +1,23 @@
 public class Cat {
-    String breed;
-    String name;
-    int age;
-    String color;
-
-    @Override
-    public String toString() {
-        return "Cat{" +
-                "breed='" + breed + '\'' +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                ", color='" + color + '\'' +
-                '}';
-    }
+//    String breed;
+//    String name;
+//    int age;
+//    String color;
+//
+//    public Cat(String breed, String name, int age, String color) {
+//        this.breed = breed;
+//        this.name = name;
+//        this.age = age;
+//        this.color = color;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Cat{" +
+//                "breed='" + breed + '\'' +
+//                ", name='" + name + '\'' +
+//                ", age=" + age +
+//                ", color='" + color + '\'' +
+//                '}';
+//    }
 }
--- FifthSeminar/src/Main.java
+++ FifthSeminar/src/Main.java
@@ -12,17 +12,29 @@ public class Main {
 //        654321 Петрова
 //        345678 Иванов
 //Вывести данные по сотрудникам с фамилией Иванов.
-        HashMap<Integer, String> hashMap = new HashMap<>();
-        hashMap.put(123456, "Иванов");
-        hashMap.put(321456, "Васильев");
-        hashMap.put(234561, "Петрова");
-        hashMap.put(234432, "Иванов");
-        hashMap.put(654321, "Петрова");
-        hashMap.put(345678, "Иванов");
-        System.out.println(hashMap);
-        for (var item:hashMap.entrySet()) {
-            if (item.getValue().equals("Иванов"))
-                System.out.println(item);
-        }
+//        HashMap<Integer, String> hashMap = new HashMap<>();
+//        hashMap.put(123456, "Иванов");
+//        hashMap.put(321456, "Васильев");
+//        hashMap.put(234561, "Петрова");
+//        hashMap.put(234432, "Иванов");
+//        hashMap.put(654321, "Петрова");
+//        hashMap.put(345678, "Иванов");
+//        System.out.println(hashMap);
+//        for (var item:hashMap.entrySet()) {
+//            if (item.getValue().equals("Иванов"))
+//                System.out.println(item);
+//        }
+
+//        Cat cat = new Cat();
+//        cat.age = 9;
+//        cat.name = "barsik";
+//        Cat cat1 = new Cat();
+//        cat1.name = "felix";
+////        CatShelter catShelter = new CatShelter();
+//        CatShelter catShelter = new SiberanCatShelter();
+//        catShelter.acceptTheCat(cat);
+//        catShelter.acceptTheCat(cat1);
+//        catShelter.printCats();
+
     }
 }
